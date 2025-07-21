357f9931a95e76e913955ec71499353ef5d8eb16
luis-studioG <luis@studiographene.com>
2025-07-21 16:00:54 +0100

    updated route in app.js, added middleware

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const express = require('express');
 const app = express();
 
 const { mongoConnect } = require('./utils/database');
+const logger = require('./middleware/logger');
 const productsRoute = require('./routes/products');
 const coursesRoute = require('./routes/courses');
 const usersRoute = require('./routes/users')
+const booksRoute = require('./routes/books');
 
+// Apply logger middleware to all routes
+app.use(logger);
 
 app.use('/products', productsRoute);
 app.use('/courses', coursesRoute);
 app.use('/users', usersRoute);
+app.use('/books', booksRoute);
 
 app.get('/', (req, res) => {
     res.send("Hello App!!!!")
